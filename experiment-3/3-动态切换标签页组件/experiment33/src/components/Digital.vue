26de0a54181100e39ca2824f2886b0ae732681e9
emetacat <1559448108@qq.com>
2025-11-21 14:40:09 +0800

    fourteenth submit

--- experiment-3/3-动态切换标签页组件/experiment33/src/components/Digital.vue
+++ experiment-3/3-动态切换标签页组件/experiment33/src/components/Digital.vue
@@ -1,37 +1,5 @@
 <template>
   <div class="content-flex">
-    <GoodsCom tag="热销">
-      <template v-slot:header>
-        <img
-          class="product-img"
-          src="../assets/computer.jpeg"
-          alt="笔记本电脑" />
-        <h3 class="product-title">笔记本电脑</h3>
-        <p class="product-desc">
-          便携性与功能实用性的平衡，能适配多场景使用需求。
-        </p>
-      </template>
-      <template v-slot:price>
-        <span class="current-price">¥3999</span>
-        <span class="original-price">¥4999</span>
-      </template>
-    </GoodsCom>
-
-    <GoodsCom tag="新品">
-      <template v-slot:header>
-        <img
-          class="product-img"
-          src="../assets/wireless.jpg"
-          alt="无线蓝牙耳机" />
-        <h3 class="product-title">无线蓝牙耳机</h3>
-        <p class="product-desc">高音质，长续航，舒适佩戴体验。</p>
-      </template>
-      <template v-slot:price>
-        <span class="current-price">¥299</span>
-        <span class="original-price">¥399</span>
-      </template>
-    </GoodsCom>
-
     <GoodsCom tag="爆款">
       <template v-slot:header>
         <img class="product-img" src="../assets/mac.jpg" alt="MacBook Air" />
